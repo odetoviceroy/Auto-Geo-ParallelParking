5b4561c4edcf3567a7b770505c50febd8e94d091
George Juarez <jjuarez@albany.edu>
2018-04-12 08:18:03 -0400

    NEED TO CLEAN UP THIS CODE :horse:

--- carsim.py
+++ carsim.py
@@ -19,15 +19,17 @@ class Coordinate:
 def get_turn_radius(steer_ang):
 	return 1 / m.tan(steer_ang) # this is in radians
 
-def temp(parkspace_len, turn_radius, c1): 
-	theta = m.pi + m.asin(parkspace_len/ (2 * turn_radius))
+def get_theta(parkspace_len, turn_radius):
+	return m.pi + m.asin(parkspace_len/ (2 * turn_radius))
+
+def get_middlept(parkspace_len, turn_radius, c1): 
+	theta = get_theta(parkspace_len, turn_radius)
 	arc_length = theta * turn_radius
 	print "PARK SPACE LEN: ", parkspace_len
 	print "TURN RADIUS: ", turn_radius
 	print "THETA: ", theta
 	print "ARC LENGTH: ", arc_length
 	print "END COORDINATE OF C1 (", c1.x, ",", c1.y, ")"
-
 
 	xm = Coordinate(c1.x + (turn_radius * m.cos(theta)), c1.y + (turn_radius * m.sin(theta) ))
 	print "END COORDINATE OF XM: (", xm.x, ", ", xm.y, " )"
@@ -40,6 +42,19 @@ def plot_graph(movecar_axlept, frontcar_up, frontcar_down, backcar_up, backcar_d
 	plt.xlabel('X')
 	plt.ylabel('Y')
 	plt.autoscale()
+
+def gen_xy_vals(c1, c2, theta, turn_radius):
+	time = [x*0.2 for x in range(0, 100)]
+	theta_list = []
+	x_vals = []
+	y_vals = []
+	for t in time:
+		theta_list.append(t * (theta)/10)
+	for i in range(0, len(theta_list)):
+		if(theta_list[i] <= theta and theta_list[i] >= m.pi/2):
+			x_vals.append(c1.x + turn_radius * m.cos(theta_list[i]))
+			y_vals.append(c1.y + turn_radius * m.sin(theta_list[i]))
+	return [x_vals, y_vals]    
 
 def robot_draw_circle(radius, center):
     time = [x*0.2 for x in range(0, 51)]
@@ -71,9 +86,9 @@ ax = plt.axes(xlim = (-8,8), ylim = (-8,8))
 N = 2
 points = ax.plot(*([[], []]*N), color = 'green', linestyle ='--', marker = 'o')
 
-def animate(i):
-    points[0].set_data([i],[i])
-    points[1].set_data([i+1],[i+1])
+def animate(i, arc1x_vals, arc1y_vals):
+    points[0].set_data([0],[0])
+    points[1].set_data([arc1x_vals[i]],[arc1y_vals[i]])
     
     return points
 
@@ -95,7 +110,7 @@ if __name__ == "__main__": # main function
 
 	parkspace_len = frontcar_up.x - backcar_up.x
 
-	xm = temp(parkspace_len, turn_radius, c1)
+	xm = get_middlept(parkspace_len, turn_radius, c1)
 	xf = Coordinate(0,(backcar_up.y - backcar_down.y) /2)
 
 	c2 = Coordinate( c2_x(turn_radius,xm,xf), c2_y(turn_radius,xm,xf) )
@@ -105,6 +120,9 @@ if __name__ == "__main__": # main function
 	[x_vals, y_vals] = robot_draw_circle(turn_radius, c1)
 
 	[x_vals2, y_vals2] = robot_draw_circle(turn_radius, c2)
+
+	theta = get_theta(parkspace_len, turn_radius)
+	[arc1x_vals, arc1y_vals] = gen_xy_vals(c1, c2, theta, turn_radius)
 	plt.plot(x_vals,y_vals, 'bo')
 	plt.plot(x_vals2,y_vals2, 'bo')
 
@@ -114,10 +132,14 @@ if __name__ == "__main__": # main function
 	plt.plot([c1.x],[c1.y], "mo")
 	plt.plot([c2.x],[c2.y], "mo")
 
+	plt.plot([arc1x_vals],[arc1y_vals], "m+")
 	plt.xlabel('X')
 	plt.ylabel('Y')
 
-	ani = animation.FuncAnimation(fig, animate, init_func=init, frames=10, interval=100, blit=True)
+	ani = animation.FuncAnimation(fig, animate, init_func=init, frames=len(arc1x_vals), fargs = (
+		arc1x_vals, 
+		arc1y_vals,
+		), interval=100, blit=True)
 
 
 	plt.show()
