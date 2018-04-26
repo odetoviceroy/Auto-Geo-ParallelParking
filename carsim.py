94c3e5894dc9ee57802a464cc51f09e660070e65
George Juarez <jjuarez@albany.edu>
2018-04-25 21:50:03 -0400

    Update carsim.py

--- carsim.py
+++ carsim.py
@@ -11,11 +11,14 @@ class Coordinate:
 
 class Car:
 	def __init__(self, front_up, front_down, back_up, back_down):
-		self.front_up = front_up
-		self.front_down = front_down
-		self.back_up = back_up
-		self.back_down = back_down
-
+		self.f_up = front_up
+		self.f_down = front_down
+		self.b_up = back_up
+		self.b_down = back_down
+	def genGraphPts(self):
+		return [
+		[self.f_up.x, self.f_down.x, self.b_up.x, self.b_down.x],
+		[self.f_up.y, self.f_down.y, self.b_up.y, self.b_down.y] ]
 
 # What we are given: center point of the back axle of the moving car: movecar_axlept
 # D = length of the parking space: parkspace_len
@@ -29,14 +32,9 @@ class Car:
 # We can therefore define len_axle as the length between the front and back axle of moving car
 # Let's also define the width and length of the actual car, CAR_WIDTH, CAR_LENGTH
 
-movecar_axlept = Coordinate(4,10)
-
-frontcar_up = Coordinate(4,5)
-frontcar_down = Coordinate(4,.1)
-backcar_up = Coordinate(0,5)
-backcar_down = Coordinate(0,.1)
-
-
+fig = plt.figure()
+# ax = plt.axes(xlim = (-8,8), ylim = (-8,8))
+patch = patches.Rectangle((0, 0), 0, 0, fc='y')
 
 def get_turn_radius(steer_ang):
 	return 1 / m.tan(steer_ang) # this is in radians
@@ -101,10 +99,6 @@ def trace_path(arc1x_vals, arc1y_vals, arc2x_vals, arc2y_vals):
 		FINALY_VALS.append(arc2y_vals[i])
 	return [FINALX_VALS, FINALY_VALS]
 
-fig = plt.figure()
-ax = plt.axes(xlim = (-8,8), ylim = (-8,8))
-patch = patches.Rectangle((0, 0), 0, 0, fc='y')
-
 def init():
 	ax.add_patch(patch)
 	return patch,
@@ -117,9 +111,50 @@ def animate(i, FINALX_VALS, FINALY_VALS, theta):
 	return patch,
 
 if __name__ == "__main__": # main function
-	turn_radius = get_turn_radius(m.pi/9)
-	parkspace_len = frontcar_up.x - backcar_up.x
+	movecar_axlept = Coordinate(4,10)
+	len_car = 3 # static length of a car
+	# ----------- LET'S DEFINE THE COORDINATES OF THE FRONT STATIC CAR -----------
+	frontcar = Car(
+		Coordinate(0,0), Coordinate(0,0), Coordinate(4,5), Coordinate(4,.1)
+		)
 
+	frontcar.f_up.x = frontcar.b_up.x + len_car
+	frontcar.f_down.x = frontcar.b_down.x + len_car
+	frontcar.f_up.y = frontcar.b_up.y
+	frontcar.f_down.y = frontcar.b_down.y
+	# ----------------------------------------------------------------------------
+	# ---------- AND NOW THE COORDINATES OF THE BACK STATIC CAR ------------------
+	backcar = Car(
+		Coordinate(0,5), Coordinate(0,.1), Coordinate(0,0), Coordinate(0,0)
+		)
+	backcar.b_up.x = backcar.f_up.x - len_car
+	backcar.b_down.x = backcar.f_down.x - len_car
+	backcar.b_up.y = backcar.f_up.y
+	backcar.b_down.y = backcar.f_down.y
+	# ----------------------------------------------------------------------------
+	parkspace_len = frontcar.b_up.x - backcar.f_up.x # DEFINE PARKING SPACE LENGTH
+	width_car = frontcar.b_up.y - frontcar.b_down.y
+	len_moveto_fup = 5 # LENGTH OF BACK OF MOVING CAR TO FRONT STATIC CAR
+	bottom_coordy = frontcar.b_up.y + len_moveto_fup
+	# --------- DEFINE ALL PRELIMINARY COMPUTATIONS OF THE MOVING CAR ------------
+	movecar = Car(
+		Coordinate(0,0), Coordinate(0,0), Coordinate(0,0), Coordinate(0,0)
+	)
+
+
+	[fx, fy] = frontcar.genGraphPts()
+	[bx, by] = backcar.genGraphPts()
+	[move_x, move_y] = movecar.genGraphPts()
+
+	plt.plot([fx],[fy], "mo")
+	plt.plot([bx],[by], "go")
+	plt.plot([move_x],[move_y], "ro")
+
+	plt.show()
+	#turn_radius = get_turn_radius(m.pi/9)
+	#
+
+	'''
 	c1 = Coordinate(movecar_axlept.x, movecar_axlept.y - turn_radius)
 	theta = get_theta(parkspace_len, turn_radius)
 	xm = get_middlept(parkspace_len, turn_radius, c1, theta)
@@ -148,4 +183,4 @@ if __name__ == "__main__": # main function
 		theta,
 		), interval=100, blit=True)
 	plt.show()
-
+	'''
