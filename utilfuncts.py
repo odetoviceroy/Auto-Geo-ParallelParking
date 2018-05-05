b6ba009bc26112da19229fe3da27c7c304069da1
George Juarez <jjuarez@albany.edu>
2018-05-05 19:35:23 -0400

    Update utilfuncts.py

--- utilfuncts.py
+++ utilfuncts.py
@@ -9,17 +9,17 @@ def get_parkspace_len(frontcar, backcar):
 
 def get_turn_radius(axle_len, steer_ang):
 	res = axle_len / m.tan(steer_ang) # this is in radians
-	print "TURN RADIUS:", res
+	print "STEER_ANG:", steer_ang, "\nTURN RADIUS:", res
 	return res
 
-def gen_xf(backcar, err_len, dist_backofcar_tobackaxle, width_car, axle_len):
+def gen_xf(backcar, err_len, dist_backofcar_tobackaxle, axle_len):
 	resx = backcar.f_up.x + err_len + dist_backofcar_tobackaxle + axle_len
-	resy = err_len + (width_car / 2.0)
+	resy = err_len + (backcar.width_car / 2.0)
 	print "XF Coordinate (", resx, ",", resy, ")"
 	return Coordinate(resx,resy)
 
-def gen_delta_x(xf_x, parkspace_len, err_len, dist_backofcar_tobackaxle, axle_len):
-	res = xf_x + (parkspace_len - err_len) + dist_backofcar_tobackaxle + axle_len
+def gen_delta_x(backaxle_midpt, xf):
+	res = backaxle_midpt.x - xf.x
 	print "DELTA X:", res
 	return res
 
@@ -35,7 +35,7 @@ def gen_c1(frontcar, frontaxle_midpt, turn_radius):
 	return Coordinate(resx, resy)
 
 def gen_c2(backcar, err_len, dist_backofcar_tobackaxle, xf, turn_radius, axle_len):
-	resx = backcar.f_up.x + err_len + dist_backofcar_tobackaxle - axle_len
+	resx = xf.x
 	resy = xf.y + turn_radius
 	print "C2 Coordinate (", resx, ",", resy, ")"
 	return Coordinate(resx, resy)
@@ -45,15 +45,28 @@ def get_theta(delta_x, turn_radius):
 	print "THETA:", res
 	return res
 
-def get_middlept(c1, c2): 
-	resx = (c1.x + c2.x) /2
-	resy = (c1.y + c2.y) / 2
+def get_middlept(c1, c2, turn_radius, theta): 
+	resx = c1.x + turn_radius * m.cos(m.pi/2 + theta)
+	resy = c1.y + turn_radius * m.sin(m.pi/2 + theta)
 	print "INITIAL XM Coordinate (", resx, ",", resy, ")"
 	return Coordinate(resx, resy)
 
 def check_arcs_toobig(delta_y, turn_radius):
 	return delta_y > (2 * turn_radius)
 
+def calc_archeight(turn_radius, theta):
+	res = turn_radius - (1 - m.cos(theta / 2.0))
+	print "ARC HEIGHT:", res
+	return res
 
-def gen_angles(movecar):
-	return 0
+def check_archeight_biggerthanhalfy(arc_height, delta_y):
+	return arc_height <= (.5 * delta_y)
+
+def gen_angles(FINALX_VALS, FINALY_VALS):
+	car_angs = []
+	car_angs.append(0.0)
+	for i in range(0, len(FINALX_VALS) - 1):
+		rise = m.fabs(FINALY_VALS[i] + FINALY_VALS[i+1])
+		run = m.fabs(FINALX_VALS[i] + FINALX_VALS[i+1])
+		car_angs.append(m.atan(rise / run))
+	return car_angs
