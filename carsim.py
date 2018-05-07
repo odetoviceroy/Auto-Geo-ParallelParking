e0649b578c2f8180847afff0e3423fc7fac73477
George Juarez <jjuarez@albany.edu>
2018-05-07 19:38:44 -0400

    FIXED ANIMATION FUNCTION KINDA :cat:

--- carsim.py
+++ carsim.py
@@ -1,5 +1,6 @@
 import math as m
 import numpy as np
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import matplotlib.patches as patches
@@ -33,23 +34,80 @@ def animate(i, FINALX_VALS, FINALY_VALS, theta, f_bup, f_bdown, b_bup, b_bdown, 
 '''
 
 fig, ax = plt.subplots()
+#ax = plt.axes(xlim = (0,3), ylim = (0,3))
 xfixdata, yfixdata = 14, 8
 xdata, ydata = 5, None
 ln, = plt.plot([], [], 'ro-', animated=True)
+patch = patches.Rectangle((0, 0), 5, 5, fc='indianred', alpha = .5)	
+
 #plt.plot([xfixdata], [yfixdata], 'bo', ms=10)
 
 def init():
+	'''
 	ax.set_xlim(-1, 3)
 	ax.set_ylim(-1, 3)
-	return ln,
+	'''
+	ax.set_aspect(1)
+	ax.add_patch(patch)
+	return patch,
 
-def update(frame, movecar, FINALX_VALS, FINALY_VALS, car_angs):
+def update(i, movecar, FINALX_VALS, FINALY_VALS, car_angs, dist_bottomcar_to_axlemidpt, dist_backofcar_tofrontaxle):
+	'''
 	ln.set_data([FINALX_VALS[frame]],[FINALY_VALS[frame]])
 	movecar.setOrientation(car_angs[frame])
 	[move_x, move_y] = movecar.genGraphPts()
 	plt.plot([move_x],[move_y], "rp")
-	return ln,
 	'''
+
+	lamb_da = car_angs[i]
+	phi1 = lamb_da - m.pi/2
+	phi2 = lamb_da - m.pi
+
+	first_joint = Coordinate(FINALX_VALS[i], FINALY_VALS[i])
+	second_joint = Coordinate(
+		first_joint.x + dist_bottomcar_to_axlemidpt * m.cos(phi1),
+		first_joint.y + dist_bottomcar_to_axlemidpt * m.sin(phi1),
+		)
+	end_effector = Coordinate(
+		second_joint.x + dist_backofcar_tofrontaxle * m.cos(phi2),
+		second_joint.y + dist_backofcar_tofrontaxle * m.sin(phi2)
+
+		)
+	patch.set_width(movecar.len_car)
+	patch.set_height(movecar.width_car)
+	'''
+	ts = ax.transData
+	coords = ts.transform([end_effector.x, end_effector.y])
+	tr = mpl.transforms.Affine2D().rotate_deg_around(coords[0], coords[1], np.rad2deg(lamb_da))
+	t = ts + tr
+	print t
+	patch.set_width(movecar.len_car)
+	patch.set_height(movecar.width_car)
+
+	patch.set_transform(t)
+
+	print coords
+	'''
+
+	t2 = mpl.transforms.Affine2D().rotate_deg_around(end_effector.x, end_effector.y, np.rad2deg(lamb_da)) + ax.transData
+	patch.set_transform(t2)
+	patch.set_xy((end_effector.x, end_effector.y))
+	#print t2
+	print "lamba: ", np.rad2deg(lamb_da), "\tPHI1:",  np.rad2deg(phi1), "\tPHI2:",  np.rad2deg(phi2)
+	print "FIRST_JOINT X:",  first_joint.x, "\tFIRST_JOINT Y:", first_joint.y
+	print "SECOND_JOINT X:",  second_joint.x, "\tSECOND_JOINT Y:", second_joint.y
+	print "END EFFECTOR X:",  end_effector.x, "\tENDEFFECTOR Y:", end_effector.y, "\n-----------"
+	return patch,
+	'''
+	ts = ax.transData
+	tr = mpl.transforms.Affine2D().rotate_deg_around(0.2,0.5,10)
+	t= ts + tr
+	
+	t2 = mpl.transforms.Affine2D().rotate_deg(-45) + ax.transData
+	patch.set_transform(t2)
+	
+	
+	return patch,
     ydata = list_var_points[frame]
     ln.set_data([xfixdata,xdata], [yfixdata,ydata])
     '''
@@ -74,7 +132,7 @@ if __name__ == "__main__": # main function
 	# ----------------------------------------------------------------------------
 	# ------------- DEFINE PRELIMINARY CONSTRAINTS TO LOCATE MOVING CAR ----------
 	parkspace_len =  uf.get_parkspace_len(frontcar, backcar) # DEFINE PARKING SPACE LENGTH
-	len_moveto_fup = .3 # LENGTH OF BACK OF MOVING CAR TO FRONT STATIC CAR
+	len_moveto_fup = .45 # LENGTH OF BACK OF MOVING CAR TO FRONT STATIC CAR
 	# ----------------------------------------------------------------------------
 	# --------- DEFINE ALL PRELIMINARY COMPUTATIONS OF THE MOVING CAR ------------
 	bbotm_coordy = frontcar.b_up.y + len_moveto_fup
@@ -89,6 +147,7 @@ if __name__ == "__main__": # main function
 	# ----------- FIGURE OUT INITIAL COORDINATES OF AXLE MIDPOINTS ----------------
 	axle_len = uf.dist_twopoints(movecar.frontaxle_midpt,movecar.backaxle_midpt)
 	dist_backofcar_tobackaxle = lencar_part
+	dist_backofcar_tofrontaxle = dist_backofcar_tobackaxle + axle_len
 	dist_bottomcar_to_axlemidpt = movecar.backaxle_midpt.y - movecar.b_down.y
 	# -----------------------------------------------------------------------------b
 	# -------------------------SKETCH OUT ORIGINAL PLAN----------------------------
@@ -96,7 +155,7 @@ if __name__ == "__main__": # main function
 
 	turn_radius = uf.get_turn_radius(axle_len, steer_ang) # figure out initial turn radius
 
-	err_len = 0.1 # use this to get the closest we can get to the back car
+	err_len = 0.22 # use this to get the closest we can get to the back car
 
 	xf = uf.gen_xf(backcar, err_len, dist_backofcar_tobackaxle, axle_len) # figure out final position of midbackaxle
 
@@ -167,8 +226,9 @@ if __name__ == "__main__": # main function
 	print car_angs
 
 
+	
+	
 	'''
-	patch = patches.Rectangle((0, 0), movecar.len_car, movecar.width_car, fc='indianred')	
 	patch2 = patches.Rectangle((frontcar.b_down.x, frontcar.b_down.y), movecar.len_car, movecar.width_car, fc='rebeccapurple')
 	patch3 = patches.Rectangle((backcar.b_down.x, backcar.b_down.y), movecar.len_car, movecar.width_car, fc = 'limegreen')
 	ani = animation.FuncAnimation(fig, animate, init_func=init, frames= total_len, fargs = (
@@ -187,6 +247,8 @@ if __name__ == "__main__": # main function
 		FINALX_VALS,
 		FINALY_VALS,
 		car_angs,
+		dist_bottomcar_to_axlemidpt,
+		dist_backofcar_tofrontaxle,
 		),
 		blit=True)
 
